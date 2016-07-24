99e6fa2b8b46248fb84db3cc2578fdb06488be0f
Chonli <Chonli@users.noreply.github.com>
2016-07-24 23:22:42 +0200

    update install script

--- install.sh
+++ install.sh
@@ -3,14 +3,9 @@
 DIR_SCRIPT="/opt/cam_pi_auto/"
 
 cp ./cron/* /etc/cron.d/
-chmod +x /etc/cron.d/createvid
-chmod +x /etc/cron.d/takephoto
-
 
 cp ./www/* /var/www/html/
 
 mkdir $DIR_SCRIPT
 cp ./script/* $DIR_SCRIPT
-chmod +x $DIR_SCRIPTtakephoto
-chmod +x $DIR_SCRIPTcreatevid
 
